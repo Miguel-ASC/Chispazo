f3b7507f9353fdece4545e99a9a4bae6b3c6aab4
Jessica Angeles Resendiz Arroyo <resendizarroyojessicaangeles21@gmail.com>
2026-09-23 16:09:09 -0600

    Productos - IntelliJIDEA

--- backend/chispita/src/main/java/org/chispazo/model/Productos.java
+++ backend/chispita/src/main/java/org/chispazo/model/Productos.java
@@ -1,4 +1,141 @@
 package org.chispazo.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "productos")
 public class Productos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "descripcion", columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(name = "precio", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    @Column(name = "imagen_url", length = 255)
+    private String imagenUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categorias categoria;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DetalleCarritos> detalleCarritos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DetallesPedidos> detallesPedidos = new ArrayList<>();
+
+    // Constructor vacío (necesario para JPA)
+    public Productos() {
+    }
+
+    // Constructor con parámetros
+    public Productos(Long idProducto, String nombre, String descripcion, BigDecimal precio, Integer stock, String imagenUrl) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.imagenUrl = imagenUrl;
+    }
+
+    // Getters y Setters
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public Categorias getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categorias categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<DetalleCarritos> getDetalleCarritos() {
+        return detalleCarritos;
+    }
+
+    public void setDetalleCarritos(List<DetalleCarritos> detalleCarritos) {
+        this.detalleCarritos = detalleCarritos;
+    }
+
+    public List<DetallesPedidos> getDetallesPedidos() {
+        return detallesPedidos;
+    }
+
+    public void setDetallesPedidos(List<DetallesPedidos> detallesPedidos) {
+        this.detallesPedidos = detallesPedidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" +
+                "idProducto=" + idProducto +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                ", imagenUrl='" + imagenUrl + '\'' +
+                '}';
+    }
 }
