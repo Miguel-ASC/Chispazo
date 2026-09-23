e7b93d8d9e9e2ac47b1cdb83fff97ec2459f375b
UrielFranco <urielfranco11@outlook.com>
2026-09-23 16:30:13 -0600

    Se añade la Tabla: DetallesPedidos

--- backend/chispita/src/main/java/org/chispazo/model/DetallesPedidos.java
+++ backend/chispita/src/main/java/org/chispazo/model/DetallesPedidos.java
@@ -1,4 +1,72 @@
 package org.chispazo.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "detalles_pedidos")
 public class DetallesPedidos {
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedidos pedidos;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Productos productos;
+
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio_unitario", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(name = "subtotal", nullable = false, precision = 10, scale = 2)
+    private BigDecimal subtotal;
+
+    public DetallesPedidos() {}
+
+    // Getter & Setter
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Productos getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Productos productos) {
+        this.productos = productos;
+    }
+
+    public Pedidos getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Pedidos pedidos) {
+        this.pedidos = pedidos;
+    }
 }
+
