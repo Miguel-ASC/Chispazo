52ee7eca8cb0599666e1c622e2a9cdf60fc5d1bf
valrey <vikei.ar03@gmail.com>
2026-09-23 11:14:45 -0600

    Carpeta del backend

---
+++ backend/chispita/src/test/java/org/chispazo/ChispitaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chispazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChispitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
